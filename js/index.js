971baf27e5fe1481c24c201c9d4d60207e60ccf3
AlexShumsky <sanya.shumskiy.95@mail.ru>
2021-12-20 23:47:53 +0300

    feat: add difficulties

--- js/index.js
+++ js/index.js
@@ -2,7 +2,15 @@
 const startButton = document.querySelector('.start-b');
 const holes = document.querySelectorAll('.hole');
 const moles = document.querySelectorAll('.mole');
-
+const difficulties = document.querySelectorAll('.difficulty');
+const difficultiesList = {
+	noob: [500, 1600],
+	normal: [400, 1400],
+	hard: [300, 1200],
+	semigod: [200, 800],
+	good: [140, 450],
+}
+let difficulty = 'noob';
 let gameTimer;
 let lastHole = null;
 let isPlay = false;
@@ -10,6 +18,7 @@ let counter = 0;
 
 startButton.addEventListener('click', toggleGame);
 moles.forEach(mole => mole.addEventListener('click', hitMole));
+difficulties.forEach(difficulty => difficulty.addEventListener('click', changeDifficulty))
 
 function toggleGame() {
 	(isPlay) ? finishGame() : startGame();
@@ -23,6 +32,7 @@ function initGame() {
 	counter = 0;
 	isPlay = true;
 	gameTimer = setTimeout(() => {
+		showGreetings();
 		finishGame();
 	}, 10000);
 	startButtonText();
@@ -40,8 +50,9 @@ function getRandomHole(holes) {
 	return holes[randomIndex];
 }
 function showMoles() {
+	const timeInterval = difficultiesList[difficulty];
 	const hole = getRandomHole(holes);
-	const time = getRandomTime(300, 1200);
+	const time = getRandomTime(timeInterval[0], timeInterval[1]);
 	hole.classList.add('up');
 	setTimeout(() => {
 		hole.classList.remove('up');
@@ -67,9 +78,26 @@ function showCurrentPoints() {
 }
 function finishGame() {
 	isPlay = false;
-	alert(`Cool your result is ${counter}`);
 	counter = 0;
 	showCurrentPoints();
 	startButtonText();
 	clearTimeout(gameTimer);
+}
+function changeDifficulty() {
+	changeActiveInMenu(this);
+	changeGameDifficulty(this);
+}
+function changeActiveInMenu(active) {
+	difficulties.forEach(difficulty => difficulty.classList.remove('active'));
+	active.classList.add('active');
+}
+function changeGameDifficulty(userDifficulty) {
+	difficulty = userDifficulty.textContent.toLowerCase();
+	finishGame();
+}
+function showGreetings() {
+	let letter = (!counter) ? `Ups.. Seems like it's realy hard for you. Try another difficulty` :
+		(counter <= 2) ? `Your result is ${counter}! Definitely you can better` :
+			(counter <= 7) ? `Your result is ${counter}! Impressive, don't you won't to show us more?` : `WOW! Your result is ${counter}! You a ROCK ; ) Try another difficulty`;
+	alert(letter);
 }

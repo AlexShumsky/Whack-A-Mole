8d97154d705bbdeec36652eaa47a139148f3ecfb
AlexShumsky <sanya.shumskiy.95@mail.ru>
2021-12-21 21:21:10 +0300

    feat: add difficulty to local storage

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@
 const startButton = document.querySelector('.start-b');
 const holes = document.querySelectorAll('.hole');
 const moles = document.querySelectorAll('.mole');
+const score = document.querySelector('.top-score span');
 const difficulties = document.querySelectorAll('.difficulty');
 const difficultiesList = {
 	noob: [500, 1600],
@@ -10,34 +11,35 @@ const difficultiesList = {
 	semigod: [200, 800],
 	good: [140, 450],
 }
-let difficulty = 'noob';
-let gameTimer;
-let lastHole = null;
 let isPlay = false;
 let counter = 0;
+let lastHole = null;
+startButton.addEventListener('click', startGame);
 
-startButton.addEventListener('click', toggleGame);
 moles.forEach(mole => mole.addEventListener('click', hitMole));
 difficulties.forEach(difficulty => difficulty.addEventListener('click', changeDifficulty))
-
-function toggleGame() {
-	(isPlay) ? finishGame() : startGame();
-}
+window.addEventListener('load', changeUserScore);
+window.addEventListener('load', getLocalUserDifficulty);
+/**START GAME**/
 function startGame() {
+	disableStartButton();
 	initGame();
+	showCurrentPoints();
 	showMoles();
 }
-/**INIT**/
+
 function initGame() {
 	counter = 0;
 	isPlay = true;
-	gameTimer = setTimeout(() => {
+
+	setTimeout(() => {
 		showGreetings();
+		checkStorage();
+		changeUserScore();
 		finishGame();
 	}, 10000);
-	startButtonText();
 }
-/**SHOWMOLES**/
+
 function getRandomTime(min, max) {
 	return Math.floor(Math.random() * (max - min) + min);
 }
@@ -50,6 +52,7 @@ function getRandomHole(holes) {
 	return holes[randomIndex];
 }
 function showMoles() {
+	const difficulty = localStorage.getItem('difficulty');
 	const timeInterval = difficultiesList[difficulty];
 	const hole = getRandomHole(holes);
 	const time = getRandomTime(timeInterval[0], timeInterval[1]);
@@ -67,37 +70,67 @@ function hitMole() {
 		this.parentNode.classList.remove('up');
 	}
 }
-/**STARTBUTTON**/
-function startButtonText() {
-	(isPlay) ? startButton.textContent = 'Finish' : startButton.textContent = 'Start';
+/**GREETINGS**/
+function showGreetings() {
+	let letter = (!counter) ? `Ups.. Seems like it's realy hard for you. Try another difficulty` :
+		(counter <= 4) ? `Your result is ${counter}! Definitely you can better` :
+			(counter <= 8) ? `Your result is ${counter}! Impressive, don't you won't to show us more?` : `WOW! Your result is ${counter}! You a ROCK ; ) Try another difficulty`;
+	alert(letter);
+}
+/**STORAGE**/
+function checkStorage() {
+	(localStorage.getItem('topScore')) ? changeStorageScore() :
+		(localStorage.setItem('topScore', 0), changeStorageScore());
+}
+function getLocalUserDifficulty() {
+	(localStorage.getItem('difficulty')) ? initDifficulty() :
+		(localStorage.setItem('difficulty', 'noob'));
+}
+function changeStorageScore() {
+	let topScore = localStorage.getItem('topScore');
+	if (counter > topScore) {
+		localStorage.setItem('topScore', counter);
+	}
+}
+function changeUserScore() {
+	if (localStorage.getItem('topScore')) score.textContent = localStorage.getItem('topScore');
+}
+/**FINISH**/
+function finishGame() {
+	isPlay = false;
+	counter = 0;
+	enableStartButton();
 }
 /**COUNTER**/
 function showCurrentPoints() {
 	const countSpan = document.querySelector('span');
 	countSpan.textContent = counter;
 }
-function finishGame() {
-	isPlay = false;
-	counter = 0;
-	showCurrentPoints();
-	startButtonText();
-	clearTimeout(gameTimer);
-}
+/**DIFFICULTY**/
 function changeDifficulty() {
 	changeActiveInMenu(this);
 	changeGameDifficulty(this);
+	function changeActiveInMenu(active) {
+		difficulties.forEach(difficulty => difficulty.classList.remove('active'));
+		active.classList.add('active');
+	}
+	function changeGameDifficulty(userDifficulty) {
+		localStorage.setItem('difficulty', userDifficulty.textContent.toLowerCase())
+	}
 }
-function changeActiveInMenu(active) {
-	difficulties.forEach(difficulty => difficulty.classList.remove('active'));
-	active.classList.add('active');
+function initDifficulty() {
+	let localDifficulty = localStorage.getItem('difficulty');
+	difficulties.forEach(difficulty => {
+		if (difficulty.textContent.toLowerCase() == localDifficulty) {
+			difficulties.forEach(difficulty => difficulty.classList.remove('active'));
+			difficulty.classList.add('active');
+		}
+	});
 }
-function changeGameDifficulty(userDifficulty) {
-	difficulty = userDifficulty.textContent.toLowerCase();
-	finishGame();
-}
-function showGreetings() {
-	let letter = (!counter) ? `Ups.. Seems like it's realy hard for you. Try another difficulty` :
-		(counter <= 2) ? `Your result is ${counter}! Definitely you can better` :
-			(counter <= 7) ? `Your result is ${counter}! Impressive, don't you won't to show us more?` : `WOW! Your result is ${counter}! You a ROCK ; ) Try another difficulty`;
-	alert(letter);
-}
+/**START BUTTON**/
+function disableStartButton() {
+	startButton.removeEventListener('click', startGame);
+};
+function enableStartButton() {
+	startButton.addEventListener('click', startGame);
+};
